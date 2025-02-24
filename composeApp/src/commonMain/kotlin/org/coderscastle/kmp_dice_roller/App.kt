58483f998166501c5e0ec82e5911709d5c852785
limonreza00 <limonreza3.1416@gmail.com>
2025-02-24 22:24:31 +0600

    Initial commit

--- composeApp/src/commonMain/kotlin/org/coderscastle/kmp_dice_roller/App.kt
+++ composeApp/src/commonMain/kotlin/org/coderscastle/kmp_dice_roller/App.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.R
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -20,18 +21,17 @@ import dicerollerapps.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Welcome to Dice Roller Game !",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    ,
+                fontSize = MaterialTheme.typography.h5.fontSize
+            )
         }
     }
 }
